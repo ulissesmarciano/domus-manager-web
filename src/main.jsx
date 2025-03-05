7e696a40c4ff6e108c17d8ea09421119f847a850
Ulisses <ulissesmarcianodev@gmail.com>
2025-03-05 17:21:40 -0300

    refactor: Limpeza de código para o funcionamento do módulo

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,9 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.jsx';
-import { GlobalStyle } from './styles/GlobalStyles.js';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
-    <GlobalStyle />
   </StrictMode>,
 );
