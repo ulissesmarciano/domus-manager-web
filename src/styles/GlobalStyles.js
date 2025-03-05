6c3c343b2fc8dbe97076c40b82b60b9036e1443d
Ulisses <ulissesmarcianodev@gmail.com>
2025-03-05 17:30:28 -0300

    style: elemento de transição suave dark/light

--- src/styles/GlobalStyles.js
+++ src/styles/GlobalStyles.js
@@ -12,6 +12,7 @@ export const GlobalStyle = createGlobalStyle`
     body {
         background-color: ${({ theme }) => theme.background};
         color: ${({ theme }) => theme.text};
+        transition: background-color 0.3s ease, color 0.3s ease;
     }
 
     a {
